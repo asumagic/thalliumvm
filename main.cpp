8975fd83692a7810d48262b7c32e4ee28f6e93fb
AsuMagic <asusylvain@hotmail.fr>
2016-10-27 20:57:14 +0200

    updated test program

--- main.cpp
+++ main.cpp
@@ -3,10 +3,13 @@
 int main()
 {
     using namespace thallium;
-	VM vm{1024};
+	VM vm{128};
 	vm.import_program({
-		{Opcode::imm, 0x0000000AFFFFFFFF},
-		{Opcode::mov, 0x00000000000B000A},
+		{Opcode::imm,  0x0000000AFFFFFFFF}, // imm FFFFFFFF %r10
+		{Opcode::push, 0x000000000000000A}, // push %r10
+		{Opcode::pop,  0x000000000000000B}, // pop %r11
+		{Opcode::teq,  0x00000000000B000A}, // teq %r10 %r11
+		{Opcode::cjmp, 8 * Instruction::size()}, // cjmp somewhere
 		{Opcode::__PLACEHOLDER_EXIT, 0}
 	});
     vm.run();
